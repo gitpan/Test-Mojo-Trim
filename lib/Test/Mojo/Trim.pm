b2fc137d4576f6dfe243fbfab6030c8ca694f312
Erik Carlsson <info@code301.com>
2014-09-04 14:46:22 +0000

    Import of CSSON/Test-Mojo-Trim-0.04 from CPAN.
    gitpan-cpan-distribution: Test-Mojo-Trim
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         CSSON/Test-Mojo-Trim-0.04.tar.gz
    gitpan-cpan-author:       CSSON
    gitpan-cpan-maturity:     released

--- lib/Test/Mojo/Trim.pm
+++ lib/Test/Mojo/Trim.pm
@@ -4,7 +4,7 @@ use strict;
 
 use Mojo::Base 'Test::Mojo';
 use Mojo::Util 'squish';
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub trimmed_content_is {
     my $self = shift;
@@ -12,9 +12,13 @@ sub trimmed_content_is {
     my $desc = shift;
 
     my $got = squish($self->tx->res->dom->to_string);
+    my $error = defined $self->tx->res->dom->find('#error') ? $self->tx->res->dom->find('#error')->text : undef;
+    chomp $error;
+
     $value =~ s{> <}{><}g;
     $got =~ s{> <}{><}g;
     $desc ||= 'exact match for trimmed content';
+    $desc .= (defined $error ? " (Error: $error)" : '');
 
     return $self->_test('is', $got, $value, $desc);
 }
